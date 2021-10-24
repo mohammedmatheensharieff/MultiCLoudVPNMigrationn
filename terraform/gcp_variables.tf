d2037fa8aa4b749eedcdfe384bc1c45513516102
Mohammed Matheen <42526574+mohammedmatheensharieff@users.noreply.github.com>
2021-10-24 16:02:47 +0530

    default change

--- terraform/gcp_variables.tf
+++ terraform/gcp_variables.tf
@@ -17,7 +17,6 @@ variable "gcp_region" {
 variable "gcp_instance_type" {
   description = "Machine Type. Correlates to an network egress cap."
   default     = "n1-standard-1"
-  default     = "n1-highmem-4"
 }
 
 variable "gcp_disk_image" {
